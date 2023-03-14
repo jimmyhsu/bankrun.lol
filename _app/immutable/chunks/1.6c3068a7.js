d12fcb456718e1427e74ca1df97e6d8e341150e5
Jimmy Hsu <jimmyhsu@hey.com>
2023-03-13 23:12:19 -0700

    feat: update opengraph meta tags

---
+++ _app/immutable/chunks/1.6c3068a7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.70bd7f63.js";export{t as component};
--- _app/immutable/chunks/1.7f7a3d9c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.b52d3bf8.js";export{t as component};
